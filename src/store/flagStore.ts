31d5b8ac9b6ca84d56f78dae4a896f5d56e70736
Elvis Peter <elvispeter@Elvis-MacBook-Pro.local>
2025-05-25 23:48:44 -0400

    added zustand and created app store

---
+++ src/store/flagStore.ts
@@ -0,0 +1,18 @@
+import { BtnType } from "@/lib/components/blocks/appButton/appButton";
+import { MedalData } from "@/lib/utils";
+import { create } from "zustand";
+
+export type MedalStoreType = {
+  data: MedalData[];
+  sortOption: BtnType | null;
+  setMedalData: (data: MedalData[]) => void;
+  setSortOption: (sortOption: BtnType) => void;
+};
+
+export const useMedalStore = create<MedalStoreType>((set) => ({
+  data: [],
+  sortOption: 'gold',
+  setMedalData: (data: MedalData[]) => set(() => ({ data })),
+  setSortOption: (sortOption: BtnType) => set(() => ({ sortOption })),
+}));
+
